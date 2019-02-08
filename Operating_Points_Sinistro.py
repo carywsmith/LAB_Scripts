199d4c8a73f36df83c9e3d95283223317665684a
carywsmith <csmith@lco.global>
2019-02-08 10:08:21 -0800

    Create Operating_Points_Sinistro.py
    This file is for use with a tuning script. It will allow for the camera setpoint to be adjusted to the included values. Images will be taken at those values.

---
+++ Operating_Points_Sinistro.py
@@ -0,0 +1,588 @@
+operating_point_default = {
+		# The default operating point represents modified datasheet values where possible.
+		"T_ccd": -100,
+		"od": 21.0,
+		"rd": 15.5,
+		"og": -1.5,
+		"dd": 15.0,
+		"s123": [-5.0, 5.0],
+		"sg": [-5.0, 5.0],
+		"rg": [-0.01, 9.99],
+		"p12": [-7.0, 5.00],
+		"p3": [-6.00, 4.5],
+		"tg": [-6.00, 4.5],
+		"adc_offsets": (0xc3,) * 4,
+}
+operating_point_1 = {
+	# The default operating point represents modified datasheet values where possible.
+	"T_ccd": -100,
+	"od": 23.0,
+	"rd": 16.5,
+	"og": -1.5,
+	"dd": 15.0,
+	"s123": [-5.0, 5.0],
+	"sg": [-5.0, 5.0],
+	"rg": [-0.01, 9.99],
+	"p12": [-7.0, 5.00],
+	"p3": [-6.0, 4.5],
+	"tg": [-6.0, 4.5],
+	"adc_offsets": (0xc3,) * 4,
+}
+
+operating_point_2 = {
+	# The default operating point represents modified datasheet values where possible.
+	"T_ccd": -100,
+	"od": 23.0,
+	"rd": 16.0,
+	"og": -1.5,
+	"dd": 15.0,
+	"s123": [-5.0, 5.0],
+	"sg": [-5.0, 5.0],
+	"rg": [-0.01, 9.99],
+	"p12": [-7.0, 5.00],
+	"p3": [-6.0, 4.5],
+	"tg": [-6.0, 4.5],
+	"adc_offsets": (0xc3,) * 4,
+}
+
+operating_point_3 = {
+	# The default operating point represents modified datasheet values where possible.
+	"T_ccd": -100,
+	"od": 23.0,
+	"rd": 15.5,
+	"og": -1.5,
+	"dd": 15.0,
+	"s123": [-5.0, 5.0],
+	"sg": [-5.0, 5.0],
+	"rg": [-0.01, 9.99],
+	"p12": [-7.0, 5.00],
+	"p3": [-6.0, 4.5],
+	"tg": [-6.0, 4.5],
+	"adc_offsets": (0xc3,) * 4,
+}
+
+operating_point_4 = {
+	# The default operating point represents modified datasheet values where possible.
+	"T_ccd": -100,
+	"od": 23.0,
+	"rd": 15.0,
+	"og": -1.5,
+	"dd": 15.0,
+	"s123": [-5.0, 5.0],
+	"sg": [-5.0, 5.0],
+	"rg": [-0.01, 9.99],
+	"p12": [-7.0, 5.00],
+	"p3": [-6.0, 4.5],
+	"tg": [-6.0, 4.5],
+	"adc_offsets": (0xc3,) * 4,
+}
+
+operating_point_5 = {
+	# The default operating point represents modified datasheet values where possible.
+	"T_ccd": -100,
+	"od": 23.0,
+	"rd": 14.5,
+	"og": -1.5,
+	"dd": 15.0,
+	"s123": [-5.0, 5.0],
+	"sg": [-5.0, 5.0],
+	"rg": [-0.01, 9.99],
+	"p12": [-7.0, 5.00],
+	"p3": [-6.0, 4.5],
+	"tg": [-6.0, 4.5],
+	"adc_offsets": (0xc3,) * 4,
+}
+
+operating_point_6 = {
+	# The default operating point represents modified datasheet values where possible.
+	"T_ccd": -100,
+	"od": 23.0,
+	"rd": 14.0,
+	"og": -1.5,
+	"dd": 15.0,
+	"s123": [-5.0, 5.0],
+	"sg": [-5.0, 5.0],
+	"rg": [-0.01, 9.99],
+	"p12": [-7.0, 5.00],
+	"p3": [-6.0, 4.5],
+	"tg": [-6.0, 4.5],
+	"adc_offsets": (0xc3,) * 4,
+}
+
+operating_point_7 = {
+	# The default operating point represents modified datasheet values where possible.
+	"T_ccd": -100,
+	"od": 23.0,
+	"rd": 13.5,
+	"og": -1.5,
+	"dd": 15.0,
+	"s123": [-5.0, 5.0],
+	"sg": [-5.0, 5.0],
+	"rg": [-0.01, 9.99],
+	"p12": [-7.0, 5.00],
+	"p3": [-6.0, 4.5],
+	"tg": [-6.0, 4.5],
+	"adc_offsets": (0xc3,) * 4,
+}
+operating_point_8 = {
+	# The default operating point represents modified datasheet values where possible.
+	"T_ccd": -100,
+	"od": 22.0,
+	"rd": 16.5,
+	"og": -1.5,
+	"dd": 15.0,
+	"s123": [-5.0, 5.0],
+	"sg": [-5.0, 5.0],
+	"rg": [-0.01, 9.99],
+	"p12": [-7.0, 5.00],
+	"p3": [-6.0, 4.5],
+	"tg": [-6.0, 4.5],
+	"adc_offsets": (0xc3,) * 4,
+}
+
+operating_point_9 = {
+	# The default operating point represents modified datasheet values where possible.
+	"T_ccd": -100,
+	"od": 22.0,
+	"rd": 16.0,
+	"og": -1.5,
+	"dd": 15.0,
+	"s123": [-5.0, 5.0],
+	"sg": [-5.0, 5.0],
+	"rg": [-0.01, 9.99],
+	"p12": [-7.0, 5.00],
+	"p3": [-6.0, 4.5],
+	"tg": [-6.0, 4.5],
+	"adc_offsets": (0xc3,) * 4,
+}
+
+operating_point_10 = {
+	# The default operating point represents modified datasheet values where possible.
+	"T_ccd": -100,
+	"od": 22.0,
+	"rd": 15.5,
+	"og": -1.5,
+	"dd": 15.0,
+	"s123": [-5.0, 5.0],
+	"sg": [-5.0, 5.0],
+	"rg": [-0.01, 9.99],
+	"p12": [-7.0, 5.00],
+	"p3": [-6.0, 4.5],
+	"tg": [-6.0, 4.5],
+	"adc_offsets": (0xc3,) * 4,
+}
+
+operating_point_11 = {
+	# The default operating point represents modified datasheet values where possible.
+	"T_ccd": -100,
+	"od": 22.0,
+	"rd": 15.0,
+	"og": -1.5,
+	"dd": 15.0,
+	"s123": [-5.0, 5.0],
+	"sg": [-5.0, 5.0],
+	"rg": [-0.01, 9.99],
+	"p12": [-7.0, 5.00],
+	"p3": [-6.0, 4.5],
+	"tg": [-6.0, 4.5],
+	"adc_offsets": (0xc3,) * 4,
+}
+
+operating_point_12 = {
+	# The default operating point represents modified datasheet values where possible.
+	"T_ccd": -100,
+	"od": 22.0,
+	"rd": 14.5,
+	"og": -1.5,
+	"dd": 15.0,
+	"s123": [-5.0, 5.0],
+	"sg": [-5.0, 5.0],
+	"rg": [-0.01, 9.99],
+	"p12": [-7.0, 5.00],
+	"p3": [-6.0, 4.5],
+	"tg": [-6.0, 4.5],
+	"adc_offsets": (0xc3,) * 4,
+}
+
+operating_point_13 = {
+	# The default operating point represents modified datasheet values where possible.
+	"T_ccd": -100,
+	"od": 22.0,
+	"rd": 14.0,
+	"og": -1.5,
+	"dd": 15.0,
+	"s123": [-5.0, 5.0],
+	"sg": [-5.0, 5.0],
+	"rg": [-0.01, 9.99],
+	"p12": [-7.0, 5.00],
+	"p3": [-6.0, 4.5],
+	"tg": [-6.0, 4.5],
+	"adc_offsets": (0xc3,) * 4,
+}
+
+operating_point_14 = {
+	# The default operating point represents modified datasheet values where possible.
+	"T_ccd": -100,
+	"od": 22.0,
+	"rd": 13.5,
+	"og": -1.5,
+	"dd": 15.0,
+	"s123": [-5.0, 5.0],
+	"sg": [-5.0, 5.0],
+	"rg": [-0.01, 9.99],
+	"p12": [-7.0, 5.00],
+	"p3": [-6.0, 4.5],
+	"tg": [-6.0, 4.5],
+	"adc_offsets": (0xc3,) * 4,
+}
+operating_point_15 = {
+	# The default operating point represents modified datasheet values where possible.
+	"T_ccd": -100,
+	"od": 22.0,
+	"rd": 13.0,
+	"og": -1.5,
+	"dd": 15.0,
+	"s123": [-5.0, 5.0],
+	"sg": [-5.0, 5.0],
+	"rg": [-0.01, 9.99],
+	"p12": [-7.0, 5.00],
+	"p3": [-6.0, 4.5],
+	"tg": [-6.0, 4.5],
+	"adc_offsets": (0xc3,) * 4,
+}
+
+operating_point_16 = {
+	# The default operating point represents modified datasheet values where possible.
+	"T_ccd": -100,
+	"od": 21.0,
+	"rd": 16.5,
+	"og": -1.5,
+	"dd": 15.0,
+	"s123": [-5.0, 5.0],
+	"sg": [-5.0, 5.0],
+	"rg": [-0.01, 9.99],
+	"p12": [-7.0, 5.00],
+	"p3": [-6.0, 4.5],
+	"tg": [-6.0, 4.5],
+	"adc_offsets": (0xc3,) * 4,
+}
+
+operating_point_17 = {
+	# The default operating point represents modified datasheet values where possible.
+	"T_ccd": -100,
+	"od": 21.0,
+	"rd": 16.0,
+	"og": -1.5,
+	"dd": 15.0,
+	"s123": [-5.0, 5.0],
+	"sg": [-5.0, 5.0],
+	"rg": [-0.01, 9.99],
+	"p12": [-7.0, 5.00],
+	"p3": [-6.0, 4.5],
+	"tg": [-6.0, 4.5],
+	"adc_offsets": (0xc3,) * 4,
+}
+
+operating_point_18 = {
+	# The default operating point represents modified datasheet values where possible.
+	"T_ccd": -100,
+	"od": 21.0,
+	"rd": 15.5,
+	"og": -1.5,
+	"dd": 15.0,
+	"s123": [-5.0, 5.0],
+	"sg": [-5.0, 5.0],
+	"rg": [-0.01, 9.99],
+	"p12": [-7.0, 5.00],
+	"p3": [-6.0, 4.5],
+	"tg": [-6.0, 4.5],
+	"adc_offsets": (0xc3,) * 4,
+}
+
+operating_point_19 = {
+	# The default operating point represents modified datasheet values where possible.
+	"T_ccd": -100,
+	"od": 21.0,
+	"rd": 15.0,
+	"og": -1.5,
+	"dd": 15.0,
+	"s123": [-5.0, 5.0],
+	"sg": [-5.0, 5.0],
+	"rg": [-0.01, 9.99],
+	"p12": [-7.0, 5.00],
+	"p3": [-6.0, 4.5],
+	"tg": [-6.0, 4.5],
+	"adc_offsets": (0xc3,) * 4,
+}
+
+operating_point_20 = {
+	# The default operating point represents modified datasheet values where possible.
+	"T_ccd": -100,
+	"od": 21.0,
+	"rd": 14.5,
+	"og": -1.5,
+	"dd": 15.0,
+	"s123": [-5.0, 5.0],
+	"sg": [-5.0, 5.0],
+	"rg": [-0.01, 9.99],
+	"p12": [-7.0, 5.00],
+	"p3": [-6.0, 4.5],
+	"tg": [-6.0, 4.5],
+	"adc_offsets": (0xc3,) * 4,
+}
+
+operating_point_21 = {
+	# The default operating point represents modified datasheet values where possible.
+	"T_ccd": -100,
+	"od": 21.0,
+	"rd": 14.0,
+	"og": -1.5,
+	"dd": 15.0,
+	"s123": [-5.0, 5.0],
+	"sg": [-5.0, 5.0],
+	"rg": [-0.01, 9.99],
+	"p12": [-7.0, 5.00],
+	"p3": [-6.0, 4.5],
+	"tg": [-6.0, 4.5],
+	"adc_offsets": (0xc3,) * 4,
+}
+
+operating_point_22 = {
+	# The default operating point represents modified datasheet values where possible.
+	"T_ccd": -100,
+	"od": 21.0,
+	"rd": 13.5,
+	"og": -1.5,
+	"dd": 15.0,
+	"s123": [-5.0, 5.0],
+	"sg": [-5.0, 5.0],
+	"rg": [-0.01, 9.99],
+	"p12": [-7.0, 5.00],
+	"p3": [-6.0, 4.5],
+	"tg": [-6.0, 4.5],
+	"adc_offsets": (0xc3,) * 4,
+}
+
+operating_point_23 = {
+	# The default operating point represents modified datasheet values where possible.
+	"T_ccd": -100,
+	"od": 22.5,
+	"rd": 16.5,
+	"og": -1.5,
+	"dd": 15.0,
+	"s123": [-5.0, 5.0],
+	"sg": [-5.0, 5.0],
+	"rg": [-0.01, 9.99],
+	"p12": [-7.0, 5.00],
+	"p3": [-6.0, 4.5],
+	"tg": [-6.0, 4.5],
+	"adc_offsets": (0xc3,) * 4,
+}
+
+operating_point_24 = {
+	# The default operating point represents modified datasheet values where possible.
+	"T_ccd": -100,
+	"od": 22.5,
+	"rd": 16.0,
+	"og": -1.5,
+	"dd": 15.0,
+	"s123": [-5.0, 5.0],
+	"sg": [-5.0, 5.0],
+	"rg": [-0.01, 9.99],
+	"p12": [-7.0, 5.00],
+	"p3": [-6.0, 4.5],
+	"tg": [-6.0, 4.5],
+	"adc_offsets": (0xc3,) * 4,
+}
+
+operating_point_25 = {
+	# The default operating point represents modified datasheet values where possible.
+	"T_ccd": -100,
+	"od": 22.5,
+	"rd": 15.5,
+	"og": -1.5,
+	"dd": 15.0,
+	"s123": [-5.0, 5.0],
+	"sg": [-5.0, 5.0],
+	"rg": [-0.01, 9.99],
+	"p12": [-7.0, 5.00],
+	"p3": [-6.0, 4.5],
+	"tg": [-6.0, 4.5],
+	"adc_offsets": (0xc3,) * 4,
+}
+
+operating_point_26 = {
+	# The default operating point represents modified datasheet values where possible.
+	"T_ccd": -100,
+	"od": 22.5,
+	"rd": 15.0,
+	"og": -1.5,
+	"dd": 15.0,
+	"s123": [-5.0, 5.0],
+	"sg": [-5.0, 5.0],
+	"rg": [-0.01, 9.99],
+	"p12": [-7.0, 5.00],
+	"p3": [-6.0, 4.5],
+	"tg": [-6.0, 4.5],
+	"adc_offsets": (0xc3,) * 4,
+}
+
+operating_point_27 = {
+	# The default operating point represents modified datasheet values where possible.
+	"T_ccd": -100,
+	"od": 22.5,
+	"rd": 14.5,
+	"og": -1.5,
+	"dd": 15.0,
+	"s123": [-5.0, 5.0],
+	"sg": [-5.0, 5.0],
+	"rg": [-0.01, 9.99],
+	"p12": [-7.0, 5.00],
+	"p3": [-6.0, 4.5],
+	"tg": [-6.0, 4.5],
+	"adc_offsets": (0xc3,) * 4,
+}
+
+operating_point_28 = {
+	# The default operating point represents modified datasheet values where possible.
+	"T_ccd": -100,
+	"od": 22.5,
+	"rd": 14.0,
+	"og": -1.5,
+	"dd": 15.0,
+	"s123": [-5.0, 5.0],
+	"sg": [-5.0, 5.0],
+	"rg": [-0.01, 9.99],
+	"p12": [-7.0, 5.00],
+	"p3": [-6.0, 4.5],
+	"tg": [-6.0, 4.5],
+	"adc_offsets": (0xc3,) * 4,
+}
+
+operating_point_29 = {
+	# The default operating point represents modified datasheet values where possible.
+	"T_ccd": -100,
+	"od": 22.5,
+	"rd": 13.5,
+	"og": -1.5,
+	"dd": 15.0,
+	"s123": [-5.0, 5.0],
+	"sg": [-5.0, 5.0],
+	"rg": [-0.01, 9.99],
+	"p12": [-7.0, 5.00],
+	"p3": [-6.0, 4.5],
+	"tg": [-6.0, 4.5],
+	"adc_offsets": (0xc3,) * 4,
+}
+
+operating_point_30 = {
+	# The default operating point represents modified datasheet values where possible.
+	"T_ccd": -100,
+	"od": 21.5,
+	"rd": 16.5,
+	"og": -1.5,
+	"dd": 15.0,
+	"s123": [-5.0, 5.0],
+	"sg": [-5.0, 5.0],
+	"rg": [-0.01, 9.99],
+	"p12": [-7.0, 5.00],
+	"p3": [-6.0, 4.5],
+	"tg": [-6.0, 4.5],
+	"adc_offsets": (0xc3,) * 4,
+}
+
+operating_point_31 = {
+	# The default operating point represents modified datasheet values where possible.
+	"T_ccd": -100,
+	"od": 21.5,
+	"rd": 16.0,
+	"og": -1.5,
+	"dd": 15.0,
+	"s123": [-5.0, 5.0],
+	"sg": [-5.0, 5.0],
+	"rg": [-0.01, 9.99],
+	"p12": [-7.0, 5.00],
+	"p3": [-6.0, 4.5],
+	"tg": [-6.0, 4.5],
+	"adc_offsets": (0xc3,) * 4,
+}
+
+operating_point_32 = {
+	# The default operating point represents modified datasheet values where possible.
+	"T_ccd": -100,
+	"od": 21.5,
+	"rd": 15.5,
+	"og": -1.5,
+	"dd": 15.0,
+	"s123": [-5.0, 5.0],
+	"sg": [-5.0, 5.0],
+	"rg": [-0.01, 9.99],
+	"p12": [-7.0, 5.00],
+	"p3": [-6.0, 4.5],
+	"tg": [-6.0, 4.5],
+	"adc_offsets": (0xc3,) * 4,
+}
+
+operating_point_33 = {
+	# The default operating point represents modified datasheet values where possible.
+	"T_ccd": -100,
+	"od": 21.5,
+	"rd": 15.0,
+	"og": -1.5,
+	"dd": 15.0,
+	"s123": [-5.0, 5.0],
+	"sg": [-5.0, 5.0],
+	"rg": [-0.01, 9.99],
+	"p12": [-7.0, 5.00],
+	"p3": [-6.0, 4.5],
+	"tg": [-6.0, 4.5],
+	"adc_offsets": (0xc3,) * 4,
+}
+
+operating_point_34 = {
+	# The default operating point represents modified datasheet values where possible.
+	"T_ccd": -100,
+	"od": 21.5,
+	"rd": 14.5,
+	"og": -1.5,
+	"dd": 15.0,
+	"s123": [-5.0, 5.0],
+	"sg": [-5.0, 5.0],
+	"rg": [-0.01, 9.99],
+	"p12": [-7.0, 5.00],
+	"p3": [-6.0, 4.5],
+	"tg": [-6.0, 4.5],
+	"adc_offsets": (0xc3,) * 4,
+}
+
+operating_point_35 = {
+	# The default operating point represents modified datasheet values where possible.
+	"T_ccd": -100,
+	"od": 21.5,
+	"rd": 14.0,
+	"og": -1.5,
+	"dd": 15.0,
+	"s123": [-5.0, 5.0],
+	"sg": [-5.0, 5.0],
+	"rg": [-0.01, 9.99],
+	"p12": [-7.0, 5.00],
+	"p3": [-6.0, 4.5],
+	"tg": [-6.0, 4.5],
+	"adc_offsets": (0xc3,) * 4,
+}
+
+operating_point_36 = {
+	# The default operating point represents modified datasheet values where possible.
+	"T_ccd": -100,
+	"od": 21.5,
+	"rd": 13.5,
+	"og": -1.5,
+	"dd": 15.0,
+	"s123": [-5.0, 5.0],
+	"sg": [-5.0, 5.0],
+	"rg": [-0.01, 9.99],
+	"p12": [-7.0, 5.00],
+	"p3": [-6.0, 4.5],
+	"tg": [-6.0, 4.5],
+	"adc_offsets": (0xc3,) * 4,
+}
